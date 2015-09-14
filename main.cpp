91ae3d8e2c233adaa87d44155fc62f1a2239ba52
Takasila Saichol <takkasila@outlook.com>
2015-09-15 01:01:00 +0700

    A white non-shader triangle

--- main.cpp
+++ main.cpp
@@ -36,11 +36,39 @@ int main()
 		fprintf(stderr, "Failed to init GLEW");
 		return -1;
 	}
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 #pragma endregion
 
-	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	static const GLfloat v_data [] = {
+		-1, -1, 0
+		, 1, -1, 0
+		, 0, 1, 0
+	};
+
+	GLuint v_buffer;
+	glGenBuffers(1, &v_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, v_buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(v_data), v_data, GL_STATIC_DRAW);
+
 
 	do{
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, v_buffer);
+		glVertexAttribPointer(
+			0
+			, 3
+			, GL_FLOAT
+			, GL_FALSE
+			, 0
+			, (void*) 0
+		);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDisableVertexAttribArray(0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
